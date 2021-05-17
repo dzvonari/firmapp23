6122f2f8b9e7493fcc3f71a9a165ae826d95d8e9
Denis Zvonarić <dzvonari@gmail.com>
2021-05-17 18:54:13 +0200

    Kreiranje izbori2021-zadnja2

---
+++ Script-7.sql
@@ -0,0 +1,26 @@
+drop database if exists firmapp23;
+
+create database firmapp23;
+
+use firmapp23;
+
+create table projekt(
+	sifra int not null primary key auto_increment,
+	naziv varchar(50) not null,
+	cijena decimal(18.2)
+);
+
+create table programer(
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	datumrodjenja datetime,
+	placa decimal(18.2)
+);
+
+create table sudjeluje(
+	projekt int,
+	programer varchar(50),
+	datumpocetka datetime,
+	datumkraja datetime
+);
